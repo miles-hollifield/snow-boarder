cd0458a30987822d42223ad6a9e5e2de686c118f
mfhollif <mileshollifieldgfp@gmail.com>
2024-01-19 13:19:27 -0500

    [Implementation] Adding sound effects

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,6 +7,7 @@ public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float loadDelay = 0.5f;
     [SerializeField] ParticleSystem crashEffect;
+    [SerializeField] AudioClip crashSFX;
     CircleCollider2D playerHead;
  
     void Start()
@@ -19,6 +20,7 @@ public class CrashDetector : MonoBehaviour
         if (other.gameObject.tag == "Ground" && playerHead.IsTouching(other.collider))
         {
             crashEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
             Invoke("ReloadScene", loadDelay);
         }
     }
